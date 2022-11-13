ffcff3a3c4df2747516b18ba14aafd61b24c1d3b
German Bravo Lopez <german.bravolopez@philips.com>
2022-11-14 00:02:18 +0100

    update function using generic function

--- sql_lite.cpp
+++ sql_lite.cpp
@@ -63,22 +63,7 @@ float read_garment_price(QSqlDatabase &db, QString garment, QString service)
 
 QString search_item_from_client(QSqlDatabase &db, QString item, QString client)
 {
-    QString item_text;
-    db.open();
-    QSqlQuery q;
-    q.exec("SELECT " + item + " FROM clientes WHERE nombre LIKE '" + client + "%'");
-    if (q.isSelect())
-    {
-        if (q.first())
-            item_text = q.value(0).toString();
-        else
-            qDebug() << "Client is not found in the database.";
-    }
-    else
-        qDebug() << "Query is not Select!";
-    q.clear();
-    db.close();
-    return item_text;
+    return select_from_where_like(db, item, "clientes", "nombre", client, false);
 }
 
 QString select_from_where_like(QSqlDatabase &db, QString item_to_get, QString table, QString column_to_search, QString item_to_search, bool exact_match)
