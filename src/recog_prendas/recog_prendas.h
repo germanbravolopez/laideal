68cf58a86ad8b35b538cbf215f90f24576c8ea9f
German Bravo Lopez <german.bravolopez@philips.com>
2022-12-08 01:34:38 +0100

    change tableView by tableWidget and add more functionality, issue: data lost when search multiple times

--- src/recog_prendas/recog_prendas.h
+++ src/recog_prendas/recog_prendas.h
@@ -20,7 +20,6 @@
 #define TABLE_SIZE      10
 #define TABLE_SERVICE   11
 #define TABLE_OBSERV    12
-#define TABLE_EDIT_LOCK 13
 
 namespace Ui {
 class RecogPrendas;
@@ -32,30 +31,27 @@ class RecogPrendas : public QMainWindow
 
 public:
     QSqlDatabase db;
-    QSqlQueryModel *sql_query_model = new QSqlQueryModel;
-    //QTableWidget table_widget_model;
-    //QModelIndex model_index_clicked;
+    bool is_cell_clicked = false, model_updated = false;
+    int row_clicked_cell, column_clicked_cell;
     explicit RecogPrendas(QWidget *parent = nullptr);
     ~RecogPrendas();
 
 private slots:
     void initial_settings();
     void reset_all_contents();
+    void clear_table_remove_rows();
+    void setup_model_table(QAbstractItemModel *model);
+    void check_new_search_may_proceed();
 
     void on_le_search_returnPressed();
     void on_pb_search_clicked();
     void on_pb_reset_clicked();
     void on_pb_payment_toggled(bool checked);
     void on_pb_state_toggled(bool checked);
-    void on_tableView_doubleClicked(const QModelIndex &index);
-
+    void on_table_cellDoubleClicked(int row, int column);
     void on_pb_save_clicked();
-/*
-    void setup_model_table(QAbstractItemModel *model);
-    void check_new_search_may_proceed();
-    bool compare_models();
-    void save_model_changes();
-*/
+    void on_le_obsv_editingFinished();
+
 private:
     Ui::RecogPrendas *ui;
 };
