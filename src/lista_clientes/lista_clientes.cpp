6c2b32cc4a8039efac7350fa1468c68fcc1b79ec
Germán Bravo López <45003170+germanbravolopez@users.noreply.github.com>
2022-12-12 13:14:23 +0100

    revert wrong change

--- src/lista_clientes/lista_clientes.cpp
+++ src/lista_clientes/lista_clientes.cpp
@@ -21,7 +21,7 @@ void ListaClientes::populate_table()
     {
         QSqlTableModel *model = new QSqlTableModel(this, QSqlDatabase::database("qt_sql_default_connection"));
         model->setTable("clientes");
-        model->setEditStrategy(QSqlTableModel::OnFieldChange);
+        model->setEditStrategy(QSqlTableModel::OnManualSubmit);
         model->select();
         ui->table_lista_clientes->setModel(model);
         ui->table_lista_clientes->resizeColumnsToContents();
