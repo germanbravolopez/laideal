57dc21a4ba03fe4c7c6cf2776f53e9c63c26a097
German Bravo <gebralo@hotmail.com>
2023-07-03 21:14:15 +0200

    remove sizes because they are not affecting

--- src/imprimir/imprimir.cpp
+++ src/imprimir/imprimir.cpp
@@ -69,11 +69,11 @@ QString Imprimir::create_html_ticket(bool copy_for_client)
             garment_name = sql_query_model->data(sql_query_model->index(row, TABLE_GARMENT)).toString();
         }
         ticket_garments.append("<tr>"
-                "<td style='padding:0mm; color:black; font-size:3mm; font-weight:400; font-style:normal; font-family:Calibri,sans-serif; text-align:left; border:none; height:4mm; width:7mm;'>"
+                "<td style='padding:0mm; color:black; font-weight:400; font-style:normal; font-family:Calibri,sans-serif; text-align:left; border:none;'>"
                     "&nbsp;&nbsp;&nbsp;"+ sql_query_model->data(sql_query_model->index(row, TABLE_QUANTITY)).toString() + "</td>"
-                "<td style='padding:0mm; color:black; font-size:3mm; font-weight:400; font-style:normal; font-family:Calibri,sans-serif; text-align:left; border:none; width:27mm;'>"
+                "<td style='padding:0mm; color:black; font-weight:400; font-style:normal; font-family:Calibri,sans-serif; text-align:left; border:none;'>"
                     + garment_name + "</td>"
-                "<td style='padding:0mm; color:black; font-size:3mm; font-weight:400; font-style: normal; font-family:Calibri,sans-serif; text-align:right; border:none; width:13mm;'>"
+                "<td style='padding:0mm; color:black; font-weight:400; font-style: normal; font-family:Calibri,sans-serif; text-align:right; border:none;'>"
                     + QString::number(sql_query_model->data(sql_query_model->index(row, TABLE_PRICE)).toFloat(), 'f', 2) + "&emsp;</td>"
             "</tr>");
         ticket_total_f = ticket_total_f + sql_query_model->data(sql_query_model->index(row, TABLE_PRICE)).toFloat();
@@ -95,11 +95,11 @@ QString Imprimir::create_html_ticket(bool copy_for_client)
         // Add copy for client or for store info
         if (copy_for_client)
         {
-            receipt_info = "<tr><td colspan='3' style='font-size:2mm;'>(Copia para el cliente)</td></tr>";
+            receipt_info = "<tr><td colspan='3'>(Copia para el cliente)</td></tr>";
         }
         else
         {
-            receipt_info = "<tr><td colspan='3' style='font-size:2mm;'>(Copia para el establecimiento)</td></tr>";
+            receipt_info = "<tr><td colspan='3'>(Copia para el establecimiento)</td></tr>";
         }
     }
     else
@@ -130,16 +130,27 @@ QString Imprimir::create_html_ticket(bool copy_for_client)
     text.append(
             "<!DOCTYPE html>"
             "<html>"
-                "<head>"
-                    "<style>"
-                        "table, th, td { border-collapse:collapse; border:none; font-size:3mm; font-weight:400; font-style:normal; font-family:Calibri,sans-serif; text-align:left; }"
-                    "</style>"
-                "</head>"
+            "<head>"
+                "<meta charset='UTF-8'>"
+                "<meta http-equiv='X-UA-Compatible' content='IE=edge'>"
+                "<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
+                "<style>"
+                    "table,tr, th, td {"
+                        "border: 1px solid black;"
+                        "border-collapse: collapse;"
+                        "border:none;"
+                        "font-weight:400;"
+                        "font-style:normal;"
+                        "font-family:Calibri,sans-serif;"
+                        "text-align:left;"
+                    "}"
+                "</style>"
+            "</head>"
             "<body>"
-                "<table style='width:72mm;'>"
+                "<table>"
                     "<tbody>"
                         "<tr>"
-                            "<td colspan='3' style='font-size:6mm; font-weight:700; text-align:center;'>Tintorer&iacute;a La Ideal"
+                            "<td colspan='3' style='font-weight:700; text-align:center;'>Tintorer&iacute;a La Ideal"
                                 "<div style='text-align:center;'>"
                                     "<span style='font-weight:700;'>Roc&iacute;o L&oacute;pez Dom&iacute;nguez</span><br>"
                                     "<span style='font-weight:700;'>NIF: 24215141-M</span><br>"
@@ -151,7 +162,7 @@ QString Imprimir::create_html_ticket(bool copy_for_client)
                         "<tr><td colspan='3'><hr></td></tr>"
                         + ticket_type +
                         "<tr>"
-                            "<td colspan='3' style='font-weight:700; text-align:right; height:5mm;'>Recibo: "
+                            "<td colspan='3' style='font-weight:700; text-align:right;'>Recibo: "
                             + ui->le_n_ticket->text() + "</td>"
                         "</tr>"
                         "<tr>"
@@ -161,13 +172,13 @@ QString Imprimir::create_html_ticket(bool copy_for_client)
                         + ticket_dates + client_address + client_id +
                         "<tr></tr>"
                         "<tr>"
-                            "<td style='font-weight:700; text-align:left; vertical-align:bottom; border-bottom:1px solid black; height:5mm; width:7mm;'>Uds.</td>"
-                            "<td style='font-weight:700; text-align:left; vertical-align:bottom; border-bottom:1px solid black; height:5mm; width:27mm;'>Prendas</td>"
-                            "<td style='font-weight:700; text-align:right; vertical-align:bottom; border-bottom:1px solid black; height:5mm; width:13mm;'>Importe</td>"
+                            "<td style='font-weight:700; text-align:left; vertical-align:bottom; border-bottom:1px solid black;'>Uds.</td>"
+                            "<td style='font-weight:700; text-align:left; vertical-align:bottom; border-bottom:1px solid black;'>Prendas</td>"
+                            "<td style='font-weight:700; text-align:right; vertical-align:bottom; border-bottom:1px solid black;'>Importe</td>"
                         "</tr>"
                         + ticket_garments +
                         "<tr>"
-                            "<td colspan='2' style='vertical-align:bottom; border-top:1px solid black; height:5mm;'>Base Imponible:</td>"
+                            "<td colspan='2' style='vertical-align:bottom; border-top:1px solid black;'>Base Imponible:</td>"
                             "<td style='text-align:right; vertical-align:bottom; border-top:1px solid black;'>"
                             + base_imponible + "&emsp;</td>"
                         "</tr>"
@@ -184,20 +195,20 @@ QString Imprimir::create_html_ticket(bool copy_for_client)
                         "<tr><td colspan='3'><hr></td></tr>"
                         "<tr></tr>"
                         "<tr>"
-                            "<td colspan='3' style='font-size:2.3mm; vertical-align:bottom;'>CONDICIONES GENERALES.</td>"
+                            "<td colspan='3' style='vertical-align:bottom;'>CONDICIONES GENERALES.</td>"
                         "</tr>"
                         "<tr>"
-                            "<td colspan='3' style='font-size:2mm; vertical-align:bottom; height:10mm;'>- El recibo deber&aacute; ser presentado al retirar la prenda. En caso de p&eacute;rdida, el usuario acreditar&aacute; su identidad.</td>"
+                            "<td colspan='3' style='vertical-align:bottom;'>- El recibo deber&aacute; ser presentado al retirar la prenda. En caso de p&eacute;rdida, el usuario acreditar&aacute; su identidad.</td>"
                         "</tr>"
                         "<tr>"
-                            "<td colspan='3' style='font-size:2mm; vertical-align:bottom; height:10mm;'>- La obligaci&oacute;n de conservar las prendas por el establecimiento caduca una vez transcurridos SEIS MESES desde la fecha de recogida.</td>"
+                            "<td colspan='3' style='vertical-align:bottom;'>- La obligaci&oacute;n de conservar las prendas por el establecimiento caduca una vez transcurridos SEIS MESES desde la fecha de recogida.</td>"
                         "</tr>"
                         "<tr>"
-                            "<td colspan='3' style='font-size:2mm; vertical-align:bottom; height:10mm;'>- No se responde de botones y otros adornos delicados de las prendas. Se recomienda que sean desmontados por el cliente.&nbsp;</td>"
+                            "<td colspan='3' style='vertical-align:bottom;'>- No se responde de botones y otros adornos delicados de las prendas. Se recomienda que sean desmontados por el cliente.&nbsp;</td>"
                         "</tr>"
                         "<tr><td colspan='3'><hr></td></tr>"
                         "<tr>"
-                            "<td colspan='3' style='font-size:2mm; vertical-align:bottom;'>"
+                            "<td colspan='3' style='vertical-align:bottom;'>"
                             + QDateTime::currentDateTime().toString("dd/MM/yyyy - hh:mm:ss") + "</td>"
                         "</tr>"
                         + receipt_info +
@@ -217,6 +228,8 @@ void Imprimir::print_ticket(QString html_text)
     QPrinter printer(QPrinter::PrinterResolution);
     printer.setPrinterName("EPSON TM-T20III");
     printer.setColorMode(QPrinter::GrayScale);
+    printer.setOutputFormat(QPrinter::NativeFormat);
+    ticketContent.print(&printer);
 }
 
 void Imprimir::on_bb_ok_cancel_accepted()
