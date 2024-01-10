9f8c0b35af8d428b24e638ac92502e2359047677
German Bravo Lopez <gebralo@hotmail.com>
2024-01-10 16:08:03 +0100

    add print script and excel

---
+++ NO_TOCAR_ticket_imprimir/print_ticket.bat
@@ -0,0 +1,20 @@
+@echo off
+
+echo Start printing Excel file...
+
+REM Launch the Windows Script Host
+cscript //nologo //B "%~F0?.wsf"
+exit /b
+
+<job id="PrintJob">
+   <script language="VBScript">
+   Set objExcel = CreateObject("Excel.Application")
+   objExcel.Visible = False
+
+   Set objWorkbook = objExcel.Workbooks.Open("C:\Users\Usuario\OneDrive\Desktop\Tintoreria\NO_TOCAR_ticket_imprimir\ImprimirTicket.xlsx")
+
+   objWorkbook.PrintOut
+   objWorkbook.Close False
+   objExcel.Quit
+   </script>
+</job>
